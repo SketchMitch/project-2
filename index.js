3766d737a4d35c434fdf6fbc10c71f32de0c0946
Mitchell Sayre <sketch0306@gmail.com>
2018-07-07 22:44:42 -0400

    Added template for movie (Not Working)

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = {
         this.flicks = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
-        
+        this.template = document.querySelector(selectors.templateSelector)
+
         document
          .querySelector(selectors.formSelector)
          .addEventListener('submit', ev => {
@@ -13,30 +14,41 @@ const app = {
     },
 
     renderListItem(flick) {
-        const item = document.createElement('li')
+        const item = this.template.cloneNode(true)
+        item.classList.remove('template')
         item.dataset.id = flick.id
-        item.textContent = flick.name
+    
+        const nameSpan = item.querySelector('.flickName')
+        nameSpan.textContent = flick.name
+        
+        
+        //const elem = item.querySelector(".flickName")
+        //debugger  
+        //elem.textContent = flick.name
+
         return item
     },
-
+  
     handleSubmit(ev) {
         
         const f = ev.target
         const flick = {
             id: ++this.max, 
             name: f.flickName.value,
+            fav: false,
         }
         
         this.flicks.unshift(flick)
-
         const item = this.renderListItem(flick)
         this.list.insertBefore(item, this.list.firstElementChild)
         
         f.reset()
     },
+    
 }
 
     app.init({
         formSelector: '#flickForm',
         listSelector: '#flickList',
+        templateSelector: '.flick.template',
     })
